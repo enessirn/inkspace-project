5a96d45679dacf13006941930382b85996d54e18
Enes Sirin <sefsirin55@gmail.com>
2025-04-11 23:08:52 +0300

    navbar right side fixed

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Dropdown, Space, Button } from "antd";
 import {
@@ -9,7 +9,7 @@ import {
 import logout from "../utils/logout";
 import GetMeContext from "../context/GetMeContext";
 function Navbar() {
-  const {me} = useContext(GetMeContext);
+  const { me } = useContext(GetMeContext);
   console.log("getMe", me);
   const storedLogin = localStorage.getItem("isLogin");
 
@@ -39,11 +39,11 @@ function Navbar() {
       key: "3",
       label: "Logout",
       icon: <LogoutOutlined />,
-      onClick: async() => {
+      onClick: async () => {
         await logout();
       },
     },
-  ];  
+  ];
   return (
     <div className="w-full bg-background  p-5 shadow-md shadow-primary/10 flex justify-between items-center">
       <Link className="logo hover:!no-underline" to="/">
@@ -53,38 +53,42 @@ function Navbar() {
       </Link>
       {
         storedLogin == "true" ? (
-          <Link to="/create-post" className="flex gap-4 items-center hover:!no-underline">
-          <span className="write-story text-secondary hover:text-primary">
-            <i className="fa-solid fa-pen-to-square text-secondary"></i> Write
-          </span>
-          <span className="text-secondary hover:text-primary">My Stories</span>
-  
-          <Dropdown menu={{ items }}>
-            <a onClick={(e) => e.preventDefault()}>
-              <Space>
-                <div className="profile-pic flex items-center justify-center cursor-pointer">
-                  <img
-                    className="w-8 h-8 rounded-full hover:shadow-sm"
-                    src={me?.profilePicture}
-                    alt="Profile Picture"
-                  />
-                </div>
-              </Space>
-            </a>
-          </Dropdown>
-        </Link>
+          <div className="flex gap-4 w-full justify-end items-center">
+            <Link to="/create-post" className="flex gap-4 items-center hover:!no-underline">
+              <span className="write-story text-secondary hover:text-primary">
+                <i className="fa-solid fa-pen-to-square text-secondary"></i> Write
+              </span>
+            </Link>
+            <span className="text-secondary hover:text-primary">My Stories</span>
+
+            <Dropdown menu={{ items }}>
+              <div>
+                <Space>
+                  <div className="profile-pic flex items-center justify-center cursor-pointer">
+                    <img
+                      className="w-8 h-8 rounded-full hover:shadow-sm"
+                      src={me?.profilePicture}
+                      alt="Profile Picture"
+                    />
+                  </div>
+                </Space>
+              </div>
+            </Dropdown>
+          </div>
+
+
         ) : (
           <Link className="hover:!no-underline" to="/login">
-          <div className="login">
-            <Button
-              className="!bg-primary hover:!bg-primary/80 !font-bold !p-4"
-              type="primary"
-              icon={<UserOutlined />}
-            >
-              Login
-            </Button>
-          </div>
-        </Link>
+            <div className="login">
+              <Button
+                className="!bg-primary hover:!bg-primary/80 !font-bold !p-4"
+                type="primary"
+                icon={<UserOutlined />}
+              >
+                Login
+              </Button>
+            </div>
+          </Link>
         )
       }
 
