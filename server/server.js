09c2531c3b5a8cc2adf83929c24603df25979716
Enes Sirin <sefsirin55@gmail.com>
2025-04-19 22:45:19 +0300

    updated cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 mongoose
   .connect(process.env.MONGO_URI, {
